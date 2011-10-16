bac7ec98854806281604a0eb5221c7d720c43d01
oley <oley@1493b2a0-b713-4256-b4fb-d7a3bfd810ec>
2011-10-16 21:15:47 +0000

    linux compilation

--- trunk/testapp/main.cpp
+++ trunk/testapp/main.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char* argv[])
 {
   optimization_suite& opti = optimization_suite::getInstance();
-  opti.init(-6, 6, -6, 6, "(x*x+y-11)^2+(x+y*y-7)^2", 20);
+  //opti.init(-6, 6, -6, 6, "(x*x+y-11)^2+(x+y*y-7)^2", 20);
+  opti.init(-6, 6, -6, 6, "(1-x)^2+100*(y-x^2)^2", 20);
   opti.optimize();
   std::cin.get();
   return 0;
